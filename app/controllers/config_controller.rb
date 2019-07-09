212fa4caf4d5e683dde1b4b74e0ccc9c89e5d0c8
Emanuele Tamponi <emanuele.tamponi@gmail.com>
2019-07-09 17:36:16 +0200

    config: hardcode Discourse generic SVG for now

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -7,9 +7,14 @@ module KeybaseProofs
     skip_before_action :check_xhr
 
     def index
+      # using base_url proxy to make experiments, since on dev base_url is on http.
+      base_url = Discourse.base_url
+      # ... like this one:
+      # base_url = "https://example.com"
+
       render json: {
         version: 1,
-        domain: Discourse.base_url,
+        domain: base_url,
         display_name: SiteSetting.title,
 
         username: {
@@ -20,17 +25,20 @@ module KeybaseProofs
 
         brand_color: "##{ColorScheme.hex_for_name('header_background', view_context.scheme_id)}",
 
+        # Until we figure out how to provide custom SVGs, let's use these
+        # hardcoded values.
         logo: {
-          svg_full: UrlHelper.absolute(SiteSetting.site_manifest_icon_url),
+          svg_black: "https://d11a6trkgmumsb.cloudfront.net/original/3X/e/b/ebee30bd98aef20357e4a177a5a1e45b877ce088.svg",
+          svg_full: "https://d11a6trkgmumsb.cloudfront.net/original/3X/e/b/ebee30bd98aef20357e4a177a5a1e45b877ce088.svg",
         },
 
         description: SiteSetting.site_description,
 
-        prefill_url: "#{Discourse.base_url}/keybase-proofs/new-proof?kb_username=%{kb_username}&username=%{username}&sig_hash=%{sig_hash}&kb_ua=%{kb_ua}",
+        prefill_url: "#{base_url}/keybase-proofs/new-proof?kb_username=%{kb_username}&username=%{username}&sig_hash=%{sig_hash}&kb_ua=%{kb_ua}",
 
-        profile_url: "#{Discourse.base_url}/u/%{username}/summary",
+        profile_url: "#{base_url}/u/%{username}/summary",
 
-        check_url: "#{Discourse.base_url}/keybase-proofs/proofs?username=%{username}",
+        check_url: "#{base_url}/keybase-proofs/proofs?username=%{username}",
         check_path: ["signatures"],
         avatar_path: ["avatar"],
 
